7f030a766e226fa26160d5b2855a8145756a89d1
Kenji <81256452+D-Kenji@users.noreply.github.com>
2026-09-09 17:32:46 +0200

    Grote getallen
    De user kan nu getallen van meerdere cijfers op de display krijgen
    Daarbij worden de cijfers opgeslaan in een object om daarvan 1 getal te maken

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const display = document.querySelector(".displayTekst");
 const button = document.querySelector(".buttons");
 
+let creerGetal = {
+}
+let getallen= {
+
+}
+console.log(Object.values(creerGetal).length);
+
 button.addEventListener("click", (e) => {
-    if(e.target.classList.contains("nummer"))
-    console.log(e.target);
-    display.textContent = e.target.textContent;
-})
+    if(e.target.parentNode.classList.contains("nummer")){
+        console.log(e.target);
+        display.textContent += e.target.textContent
+        let index = Object.values(creerGetal).length;
+        creerGetal[index] = e.target.textContent;
+    }
+});
