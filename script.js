2479bf2b9cd3435d8eecae43569fab70c2cfbf78
Kenji <81256452+D-Kenji@users.noreply.github.com>
2026-09-11 11:56:14 +0200

    Compactere code
    Met behulp van AI heb ik mijn eerste code compacter gemaakt
    door script te hetschrijven die compacter is maar hetzelfde werkt

--- script.js
+++ script.js
@@ -1,112 +1,62 @@
 const display = document.querySelector(".displayTekst");
 const button = document.querySelector(".buttons");
 
-let creerGetal = {};
 let getallen = {};
 let resultaat;
 let soort;
 let inputbyComputer = false;
 button.addEventListener("click", (e) => {
-    let indexCreer = Object.values(creerGetal).length;
     let indexGetallen = Object.values(getallen).length;
-    console.log("Dit is de index van de getallen " + indexGetallen);
-     if(e.target.parentNode.className == "nummer"){
+     if(e.target.parentNode.className === "nummer"){
         inputbyComputer = false;
-        if(display.textContent == resultaat){
+        if(display.textContent === resultaat){
             display.textContent = "";
         }
-        display.textContent += e.target.textContent
-        creerGetal[indexCreer] = e.target.textContent;
+        display.textContent += e.target.textContent;
     }
 
-    if(e.target.parentNode.className == "berekening"){
-        if(display.textContent !== "" && inputbyComputer == false){
-            console.log("toegevoegd aan object")
+    if(e.target.parentNode.className === "berekening"){
+        if(display.textContent !== "" && inputbyComputer === false){
             getallen[indexGetallen] = parseFloat(display.textContent);
-            console.log(getallen);
             display.textContent = "";
             indexGetallen = Object.values(getallen).length;
         }
 
             switch (e.target.className){
-                case 'maal':
-                    soort = 'maal';
-                    if(indexGetallen == 2){
-                    berekening();
-                    } 
-                    break;
-                case 'plus':
-                    soort = 'plus';
-                    if(indexGetallen == 2){
-                    berekening();
-                    } 
-                    break;
-                case 'min':
-                    soort = 'min';
-                    if(indexGetallen == 2){
-                    berekening();
-                    } 
-                    break;
-                case 'delen':
-                    soort = 'delen';
-                    if(indexGetallen == 2){
-                    berekening();
-                    } 
-                    break;
+                case 'maal': soort = 'maal'; break;
+                case 'plus': soort = 'plus'; break;
+                case 'min': soort = 'min'; break;
+                case 'delen': soort = 'delen'; break;
                 case 'gelijk':
-                    if(indexGetallen == 2){
-                    berekening();
-                    } else {
+                    if(indexGetallen !== 2){
                         console.log(indexGetallen);
                         alert("Er is geen tweede getal");
                     }
                     break;
             }
+            if(soort && indexGetallen === 2){
+                berekening();
+            }
         }
     }
 );
+const bewerking = {
+    plus: (a,b) => a+b,
+    min: (a,b) => a-b,
+    maal: (a,b) => a*b,
+    delen: (a,b) => a/b,
+
+}
 
 function berekening(){
-    console.log("berekend")
-    switch (soort){
-                case 'maal':
-                    maal(getallen[0], getallen[1])
-                    break;
-                case 'plus':
-                    plus(getallen[0], getallen[1])
-                    break;
-                case 'min':
-                    min(getallen[0], getallen[1])
-                    break;
-                case 'delen':
-                    delen(getallen[0], getallen[1])
-                    break;
-            }
+    resultaat = bewerking[soort](getallen[0],getallen[1]);
+    resetGetallen();
 
-}
-
-function plus(a, b){
-    resultaat = a + b
-    resetGetallen()
-}
-function min(a, b){
-    resultaat = a - b
-    resetGetallen();
-}
-function delen(a, b){
-    resultaat = a / b
-    resetGetallen();
-}
-function maal(a, b){
-    resultaat = a * b
-    resetGetallen();
 }
 
 function resetGetallen(){
     getallen ={};
     getallen[0] = resultaat
-    console.log(getallen);
     display.textContent = resultaat;
     inputbyComputer = true;
-    console.log("Het resultaat is " + resultaat);
 }
